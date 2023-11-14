d84994f88c9036612a5e99ed69092ade8cf141d4
Ezequiel <ezequiel.h@lyncas.net>
2023-11-14 16:05:01 -0300

    stopwatch

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,62 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <div class="title">
+      <h1 class="logoTitle">S</h1>
+      <h1>StopWatch</h1>
+    </div>
+    <Display />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Display from "./components/Display.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components: { Display },
+};
 </script>
 
 <style>
+@font-face {
+  font-family: "Montserrat-Regular";
+  src: url("./assets/fonts/Montserrat-Regular.ttf");
+}
+@font-face {
+  font-family: "Montserrat-Light";
+  src: url("./assets/fonts/Montserrat-Light.ttf");
+}
+@font-face {
+  font-family: "Montserrat-Thin";
+  src: url("./assets/fonts/Montserrat-Thin.ttf");
+}
+
+* {
+  padding: 0;
+  margin: 0;
+  background-color: #041f1e;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Montserrat-Regular";
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
+}
+
+.title {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 90px;
+  color: #fff;
+}
+
+.logoTitle {
+  background-color: #519872;
+  color: #fff;
+  margin-right: 10px;
+  padding: 5px 15px;
+  border-radius: 10px;
 }
 </style>
---
+++ src/components/Display.vue
@@ -0,0 +1,133 @@
+<template>
+  <div>
+    <div class="stopwatch">
+      <div class="screenWatch">
+        <input type="number" placeholder="0" v-model="hours" />
+        <input type="number" placeholder="0" v-model="minutes" />
+        <input type="number" placeholder="0" v-model="seconds" />
+      </div>
+    </div>
+    <div v-if="!counting" class="sendButton">
+      <button @click="handleTimer">Set</button>
+    </div>
+    <div v-else class="pauseButton">
+      <button @click="pauseTimer">Pause</button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Display",
+
+  data() {
+    return {
+      hours: null,
+      minutes: null,
+      seconds: null,
+
+      counting: false,
+      timerInterval: null,
+    };
+  },
+  methods: {
+    handleTimer() {
+      this.counting = true;
+
+      this.timerInterval = setInterval(() => {
+        this.handleSeconds();
+      }, 1000);
+    },
+
+    handleSeconds() {
+      if (this.seconds >= 60) {
+        this.seconds = 60;
+      }
+      if (this.minutes >= 60) {
+        this.minutes = 59;
+      }
+
+      //segundos e minutos zerados
+      if (!this.seconds && !this.minutes && !this.hours) {
+        clearInterval(this.timerInterval);
+        this.seconds = null;
+        this.minutes = null;
+        this.counting = false;
+      }
+
+      //minutos não zerados
+      if (!this.seconds && this.minutes) {
+        this.minutes--;
+        this.seconds = 60;
+      }
+
+      if (!this.seconds && !this.minutes && this.hours) {
+        this.hours--;
+        this.minutes = 59;
+        this.seconds = 60;
+      }
+
+      this.seconds--;
+    },
+
+    handleMinutes() {
+      this.minutes--;
+    },
+
+    pauseTimer() {
+      clearInterval(this.timerInterval);
+      this.counting = false;
+    },
+  },
+};
+</script>
+
+<style>
+.stopwatch {
+  display: flex;
+  justify-content: center;
+}
+
+.stopwatch .screenWatch {
+  display: flex;
+  align-items: center;
+  height: 350px;
+  width: 350px;
+  padding: 70px;
+  border-radius: 50%;
+  border: 4px solid #c57b57;
+}
+
+.stopwatch input {
+  font-size: 50px;
+  font-family: "Montserrat-Light";
+  color: #fff;
+  width: 30%;
+  border: 0;
+  text-align: end;
+}
+
+::placeholder {
+  color: #fff;
+}
+
+.sendButton,
+.pauseButton {
+  margin-top: 50px;
+}
+
+button {
+  font-family: "Montserrat-Regular";
+  font-size: 20px;
+  border: 0;
+  background-color: #519872;
+  padding: 20px;
+  border-radius: 15px;
+  width: 150px;
+  color: #fff;
+  font-weight: bold;
+}
+
+button:hover {
+  background-color: #346049;
+}
+</style>
